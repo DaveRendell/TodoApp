4b58345fe9ef64756fa143107a66a0cbd74c342a
David Rendell <davidwrendell@gmail.com>
2018-04-01 14:59:15 +1200

    Add empty side panel

--- app/src/main/java/com/dwrendell/todoapp/activities/MainActivity.java
+++ app/src/main/java/com/dwrendell/todoapp/activities/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     ToDoService toDoService;
     TodoItemAdapter adapter;
     DragListView dragListView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +52,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -137,16 +148,16 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_delete_all) {
-            toDoService.deleteAll();
-            adapter.setItemList(toDoService.getTodos());
-            adapter.notifyItemRangeRemoved(0, adapter.getItemCount());
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_delete_all:
+                toDoService.deleteAll();
+                adapter.setItemList(toDoService.getTodos());
+                adapter.notifyItemRangeRemoved(0, adapter.getItemCount());
+                return true;
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
         }
+        return super.onOptionsItemSelected(item);
     }
 }
